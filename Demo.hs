fe0368d001d8d9e7eeec481b669d537d1fa01c87
Ose Pedro <zuverliantellabrach@hotmail.com>
2020-09-01 03:21:47 +0100

    Rewrote much of MsgSys in terms of Maybe, and simplified it as much as possible.

--- Demo.hs
+++ Demo.hs
@@ -13,10 +13,10 @@ module Demo where
 import MsgSys
 
 (msgSys0,ose,pedro) = initialise
-msgSys1 = send "ok..." (Pair pedro (user ose)) msgSys0
+msgSys1 = send "ok..." (userPair pedro (asUser ose)) msgSys0
 
 alertee :: User -> UserPair
-alertee = Pair alerter
+alertee = userPair alerter
 
 alert :: String -> User -> MsgSys -> MsgSys
 alert msg user msgSys = send msg (alertee user) msgSys
@@ -27,40 +27,44 @@ alertOutOfSpace = alert "You're out of storage space"
 messageLengths :: [String] -> [Int]
 messageLengths = map length
 
--- Note: "sum" is provided by the standard library, so
+-- Note: the "sum" function that is defined in the presentation is provided by
+-- the standard library.
 sumMessageLengths :: [String] -> Int
 sumMessageLengths = sum . messageLengths
 
 usedSpace :: MsgSys -> User -> Int
-usedSpace msgSys user = sumMessageLengths (messages msgSys user)
+usedSpace msgSys user = sumMessageLengths (userMessages msgSys user)
 
--- Note: compose is defined in MsgSys.hs as:
+-- Note: composeActions is defined in MsgSys.hs as:
 --
---    compose = foldr (.) id
+--    ccomposeChanges = foldr (.) id
 --
 -- "id" is provided by the standard library. It behaves like the "emptyResult"
 -- function near the end of the presentation.
 --
--- You can use compose to compose any list of "MsgSys -> MsgSys" functions into
--- a single function. E.g. see how it is used in "initialise" below.
+-- You can use "composeActions" to compose any list of (MsgSys -> MsgSys)
+-- functions into a single function. E.g. see how it is used in "initialise"
+-- below.
 alertMultiOutOfSpace :: [User] -> MsgSys -> MsgSys
-alertMultiOutOfSpace users msgSys = compose (map alertOutOfSpace users) msgSys
+alertMultiOutOfSpace users msgSys =
+  composeActions (map alertOutOfSpace users) msgSys
 
 
+-- Note: Haskell uses indentation to define scopes, much like Python
 initialise :: (MsgSys,LoggedInUser,LoggedInUser)
 initialise = (msgSysWithMessages,ose,pedro)
   where
-  oseName = Name "Ose"
-  osePassword = Password "Ose's unguessable password"
+  oseName = name "Ose"
+  osePassword = password "Ose's unguessable password"
 
-  pedroName = Name "Pedro"
-  pedroPassword = Password "Pedro's beautiful password"
+  pedroName = name "Pedro"
+  pedroPassword = password "Pedro's beautiful password"
 
   msgSysWithUsers :: MsgSys
   msgSysWithUsers =
-    compose [
-      registerUser pedroName pedroPassword,
-      registerUser oseName osePassword
+    composeActions [
+      register pedroName pedroPassword,
+      register oseName osePassword
     ] emptyMsgSys
 
   ose = login oseName osePassword msgSysWithUsers
@@ -68,7 +72,7 @@ initialise = (msgSysWithMessages,ose,pedro)
 
   msgSysWithMessages :: MsgSys
   msgSysWithMessages =
-    compose [
-      send "I miss u ❤" (Pair ose (user pedro)),
-      send "hey" (Pair pedro (user ose))
+    composeActions [
+      send "I miss u ❤" (userPair ose (asUser pedro)),
+      send "hey" (userPair pedro (asUser ose))
     ] msgSysWithUsers
--- MsgSys.hs
+++ MsgSys.hs
@@ -3,161 +3,175 @@
     It exports all the functions and types above the word "PRIVATE" below,
     so they can be used in Demo.hs.
 -}
-
-{-# LANGUAGE FlexibleInstances #-}
-
 module MsgSys(
     emptyMsgSys,
     alerter,
+    name,
+    password,
+    register,
     login,
-    registerUser,
-    send,
-    messages,
     findUser,
     allUsers,
-    compose,
-    LoggedInUser, user, password,
-    UserPair(..),
-    Name(..),
-    Password(..),
-    User(..),
-    Displayable(..),
+    userPair,
+    send,
+    userMessages,
+    composeActions,
+    asUser,
+    display,
+    Name,
+    User,
+    Password,
+    LoggedInUser,
+    UserPair,
     MsgSys
 ) where
 
 import Data.List
 
 emptyMsgSys :: MsgSys
-emptyMsgSys = registerUser alerterName alerterPassword (MsgSys [] [])
+emptyMsgSys = register alerterName alerterPassword (MsgSys [] [])
 
 alerter :: LoggedInUser
 alerter = login alerterName alerterPassword emptyMsgSys
 
-login :: Name -> Password -> MsgSys -> LoggedInUser
-login name password msgSys = validateLogin msgSys (loginAttempt name password)
+name :: String -> Name
+name = Name
+
+password :: String -> Password
+password = Password
 
 -- Note: this function only allows "name" to be registered once. If you try
 -- to register it again, the MsgSys will not change
-registerUser :: Name -> Password -> MsgSys -> MsgSys
-registerUser name password msgSys =
-    if canRegister msgSys userPword
-    then msgSys {registeredUsers = userPword : registeredUsers msgSys}
-    else msgSys
+register :: Name -> Password -> MsgSys -> MsgSys
+register name password msgSys =
+    validateRegistration msgSys (rawCredentials name password)
 
-    where userPword = UserPassword (User name) password
-
--- Note: the message will only be sent if the login attempt succeeded and the
--- recipient is a registered User. The underscore symbol "_" is a placeholder
--- for parameters that we don't care about.
-send :: String -> UserPair -> MsgSys -> MsgSys
-send _ (Pair LoginFailed _) msgSys = msgSys
-send _ (Pair _ NonExistentUser) msgSys = msgSys
-send msg pair msgSys =
-    msgSys {allMessages = StoredMessage pair msg : allMessages msgSys}
-
-messages :: MsgSys -> User -> [String]
-messages msgSys user = map message storedMsgs
-    where storedMsgs = filter (involvesUser user) (allMessages msgSys)
+login :: Name -> Password -> MsgSys -> LoggedInUser
+login name password msgSys = validateLogin msgSys (rawCredentials name password)
 
 findUser :: MsgSys -> Name -> User
-findUser msgSys queryName = if null users then NonExistentUser else head users
-    where users = filter (== User queryName) (allUsers msgSys)
+findUser msgSys name =
+    if null users then Nothing else Just (head users)
+    where users = filter (== RawUser name) (allRawUsers msgSys)
 
 allUsers :: MsgSys -> [User]
-allUsers msgSys = map upUser (registeredUsers msgSys)
+allUsers msgSys = map Just (allRawUsers msgSys)
 
-compose :: [MsgSys -> MsgSys] -> MsgSys -> MsgSys
-compose = foldr (.) id
+userPair :: LoggedInUser -> User -> UserPair
+userPair creds user = do
+    rawCreds <- creds
+    rawUser <- user
+    Just (rawCreds, rawUser)
 
-user :: LoggedInUser -> User
-user (LoggedIn userPword) = upUser userPword
-user LoginFailed = NonExistentUser
+-- Note: the message will only be sent if the UserPair was created from a
+-- LoggedInUser and User that are both registered in "msgSys".
+-- The underscore symbol "_" is a placeholder for parameters that we don't care
+-- about.
+send :: String -> UserPair -> MsgSys -> MsgSys
+send _ Nothing msgSys = msgSys
+send msg (Just rawPair) msgSys =
+    msgSys {allMessages = storedMessage rawPair msg : allMessages msgSys}
 
-password :: LoggedInUser -> Password
-password (LoggedIn userPword) = upPassword userPword
-password LoginFailed = UndefinedPassword
+userMessages :: MsgSys -> User -> [String]
+userMessages msgSys user = map messageString storedMsgs
+    where storedMsgs = filter (involvesUser user) (allMessages msgSys)
 
-data UserPair = Pair LoggedInUser User deriving(Eq,Show)
+composeActions :: [MsgSys -> MsgSys] -> MsgSys -> MsgSys
+composeActions = foldr (.) id
+
+asUser :: LoggedInUser -> User
+asUser = fmap credsUser
+
 data Name = Name String deriving(Eq,Show)
-data Password = Password String | UndefinedPassword deriving(Eq,Show)
-data User = User Name | NonExistentUser deriving(Eq,Show)
-
-class Displayable a where
-    display :: a -> IO ()
+type User = Maybe RawUser
+data Password = Password String deriving(Eq,Show)
+type LoggedInUser = Maybe RawCredentials
+type UserPair = Maybe RawUserPair
 
 
 -- =============================================================================
--- PRIVATE
+--                                 PRIVATE
+-- =============================================================================
+-- The functions, and most of the types, below are hidden from other modules, to
+-- prevent other modules from invalidating the state of a MsgSys (e.g. by
+-- sending messages to/from unregistered Users). E.g. code in the Demo module
+-- cannot call any of the functions below - it can only use the functions and
+-- types listed in the export list (the list in parentheses at the top of this
+-- file, after "module MsgSys").
 --
--- The functions, and most of the types, below are hidden from other modules
--- (i.e. they have been deliberately omitted from the export list in parentheses
--- at the top of this file, after "module MsgSys"). E.g. code in the Demo module
--- cannot call any of these functions. However, ghci will give you full access
--- to everything in this module, if you load the module into it. To do this,
--- type ":l MsgSys".
+-- Note however that if you load this module into ghci, it will give you full
+-- access to everything. To do this, type ":l MsgSys".
 -- =============================================================================
 
-alerterPassword = Password "Extremely secure password"
-alerterName = Name "Alerter"
-alerterUser = User alerterName
+alerterName = name "Alerter"
+alerterPassword = password "Extremely secure password"
 
-sender :: UserPair -> LoggedInUser
-sender (Pair loggedInUser _) = loggedInUser
+rawCredentials :: Name -> Password -> RawCredentials
+rawCredentials name password = RawCredentials (RawUser name) password
 
-recipient :: UserPair -> User
-recipient (Pair _ user) = user
+sameUser :: User -> RawCredentials -> Bool
+sameUser user rawCreds = user == Just (credsUser rawCreds)
+
+storedMessage :: RawUserPair -> String -> StoredMessage
+storedMessage rawPair msg = StoredMessage rawPair (MessageString msg)
+
+messageString :: StoredMessage -> String
+messageString (StoredMessage _ (MessageString string)) = string
+
+allRawUsers :: MsgSys -> [RawUser]
+allRawUsers msgSys = map credsUser (allCredentials msgSys)
 
 involvesUser :: User -> StoredMessage -> Bool
-involvesUser u storedMsg =
-    let pair = userPair storedMsg
-    in  user (sender pair) == u || recipient pair == u
+involvesUser user storedMsg =
+    sameUser user (fst rawPair) || user == Just (snd rawPair)
+    where rawPair = storedRawUserPair storedMsg
 
--- This function returns true if msgSys has a UserPassword that satisfies
+-- This function returns true if "msgSys" has RawCredentials that satisfy
 -- "condition".
-hasSatisfyingUserPassword :: MsgSys -> (UserPassword -> Bool) -> Bool
-hasSatisfyingUserPassword msgSys condition =
-    any condition (registeredUsers msgSys)
+anyCredentialsSatisfy :: MsgSys -> (RawCredentials -> Bool) -> Bool
+anyCredentialsSatisfy msgSys condition = any condition (allCredentials msgSys)
 
-validateLogin :: MsgSys -> LoginAttempt -> LoggedInUser
-validateLogin msgSys (LoginAttempt userPword) =
-    if hasSatisfyingUserPassword msgSys (== userPword)
-    then LoggedIn userPword
-    else LoginFailed
+validateRegistration :: MsgSys -> RawCredentials -> MsgSys
+validateRegistration msgSys rawCreds =
+    if canRegister msgSys rawCreds
+    then msgSys {allCredentials = rawCreds : allCredentials msgSys}
+    else msgSys
 
-hasRegistered :: MsgSys -> User -> Bool
-hasRegistered msgSys queryUser = hasSatisfyingUserPassword msgSys sameUser
+-- RawCredentials can only register if the User and Password are defined (i.e.
+-- not Nothing), and the User has not already registered
+canRegister :: MsgSys -> RawCredentials -> Bool
+canRegister msgSys rawCreds = not registered
     where
-        sameUser :: UserPassword -> Bool
-        sameUser existingUserPword = queryUser == upUser existingUserPword
+    user = Just (credsUser rawCreds)
+    registered = anyCredentialsSatisfy msgSys (sameUser user)
 
--- UserPassword can only register if the User and Password are defined,
--- and the User has not already registered
-canRegister :: MsgSys -> UserPassword -> Bool
-canRegister _ (UserPassword NonExistentUser _) = False
-canRegister _ (UserPassword _ UndefinedPassword) = False
-canRegister msgSys userPword = not (hasRegistered msgSys (upUser userPword))
+validateLogin :: MsgSys -> RawCredentials -> LoggedInUser
+validateLogin msgSys rawCreds =
+    if anyCredentialsSatisfy msgSys (== rawCreds)
+    then Just rawCreds
+    else Nothing
 
+data RawUser = RawUser Name deriving(Eq,Show)
+type RawUserPair = (RawCredentials, RawUser)
 
-data LoginAttempt = LoginAttempt UserPassword deriving(Show)
-data LoggedInUser = LoggedIn UserPassword | LoginFailed deriving(Eq,Show)
+data RawCredentials =
+    RawCredentials {
+        credsUser :: RawUser,
+        credsPassword :: Password
+    } deriving(Eq,Show)
 
-loginAttempt :: Name -> Password -> LoginAttempt
-loginAttempt name password =
-    LoginAttempt (
-        UserPassword (User name) password
-    )
-
-data UserPassword =
-    UserPassword {upUser :: User, upPassword :: Password} deriving(Eq,Show)
-
+data MessageString = MessageString String deriving(Eq,Show)
 data StoredMessage =
-    StoredMessage {userPair::UserPair, message::String} deriving(Show)
+    StoredMessage {
+        storedRawUserPair :: RawUserPair,
+        storedMessageString :: MessageString
+    } deriving(Eq,Show)
 
 data MsgSys =
     MsgSys {
         allMessages :: [StoredMessage],
-        registeredUsers :: [UserPassword]
-    } deriving(Show)
+        allCredentials :: [RawCredentials]
+    } deriving(Eq,Show)
 
 
 -- =============================================================================
@@ -165,43 +179,54 @@ data MsgSys =
 -- contents of MsgSys with more readable formatting.
 -- =============================================================================
 
+class Displayable a where
+    display :: a -> IO ()
+
 instance Displayable MsgSys where
     display msgSys = do
+        display doubleLine
         putStrLn "Messages (oldest first):"
+        display doubleLine
         sequence_ displayMsgs
         putStrLn "Registered Users:"
+        display doubleLine
         sequence_ displayRegisteredUsers
-        where
-            displayMsgs :: [IO ()]
-            displayMsgs = map display (reverse (allMessages msgSys))
 
-            displayRegisteredUsers :: [IO ()]
-            displayRegisteredUsers = map (prefixDisplayLn "  ") (registeredUsers msgSys)
+        where
+        doubleLine = Separator '='
+
+        displayMsgs :: [IO ()]
+        displayMsgs = map display (reverse (allMessages msgSys))
+
+        displayRegisteredUsers :: [IO ()]
+        displayRegisteredUsers =
+            map (prefixDisplay "  ") (allCredentials msgSys)
 
 instance Displayable StoredMessage where
     display storedMsg = do
-        prefixDisplayLn "  From: " from
-        prefixDisplayLn "  To: " to
-        prefixDisplayLn "  Message: " (message storedMsg)
-        putStrLn ""
-        where (Pair from to) = userPair storedMsg
+        prefixDisplay "  From: " from
+        prefixDisplay "  To: " to
+        prefixDisplay "  Message: " (storedMessageString storedMsg)
+        display (Separator '-')
 
-instance Displayable LoggedInUser where
-    display LoginFailed = putStr "[Login attempt failed]"
-    display (LoggedIn userPword) = display userPword
+        where
+        (from, to) = storedRawUserPair storedMsg
 
-instance Displayable UserPassword where
-    display userPassword = display (upUser userPassword)
+instance Displayable RawCredentials where
+    display creds = display (credsUser creds)
 
-instance Displayable User where
-    display (User (Name name)) = putStr name
-    display NonExistentUser = putStr "[Non-existent User]"
+instance Displayable RawUser where
+    display (RawUser (Name name)) = putStrLn name
 
-instance Displayable String where
-    display string = putStr string
+instance Displayable MessageString where
+    display (MessageString string) = putStrLn string
 
-prefixDisplayLn :: Displayable a => String -> a -> IO ()
-prefixDisplayLn prefix a = do
+data Separator = Separator Char
+
+instance Displayable Separator where
+    display (Separator char) = putStrLn (replicate 80 char)
+
+prefixDisplay :: Displayable a => String -> a -> IO ()
+prefixDisplay prefix a = do
     putStr prefix
     display a
-    putStrLn ""
